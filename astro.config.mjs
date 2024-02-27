7bcf75517b5188b4a23b4e9940e3a11d6886705a
Ishaan Sinha <ishaan.sinhaa@gmail.com>
2024-02-27 13:31:09 +0000

    removed github pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  site: 'https://naahsi.github.io',
-  base: '/bdg',
-});
+export default defineConfig({});
